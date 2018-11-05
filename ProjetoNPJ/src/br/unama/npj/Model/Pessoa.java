a396a43c5c1678b5ca6a8df3e224dd89089f140b
26142608 <26142608@UNA007-L201-011.serlab.intra>
2018-11-05 17:27:56 -0300

    RF01

--- ProjetoNPJ/src/br/unama/npj/Model/Pessoa.java
+++ ProjetoNPJ/src/br/unama/npj/Model/Pessoa.java
@@ -2,6 +2,15 @@ package br.unama.npj.Model;
 
 public class Pessoa {
 	private int id;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public int getId() {
 		return id;
