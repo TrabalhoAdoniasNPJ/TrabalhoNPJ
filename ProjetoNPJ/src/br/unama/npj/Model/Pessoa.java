98272f1681b374b498e82ec3e7552dc9d48b970f
26158900 <26158900@UNA007-L201-017.serlab.intra>
2018-11-12 17:39:40 -0300

    Pessoa

--- ProjetoNPJ/src/br/unama/npj/Model/Pessoa.java
+++ ProjetoNPJ/src/br/unama/npj/Model/Pessoa.java
@@ -3,21 +3,47 @@ package br.unama.npj.Model;
 public class Pessoa {
 	private int id;
 	private String nome;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+	private int cpf;
+	private String email;
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Pessoa(int id, String nome, int cpf, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+	public Pessoa(String nome, int cpf, String email) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
 	
 }
+	
+
+	
