4bb01aa7f1acd69f3a8a41d0e605f573c3a4145f
26002084 <26002084@UNA007-L201-008.serlab.intra>
2018-11-22 18:05:52 -0200

    Menu

--- ProjetoNPJ/src/br/unama/npj/Model/ParteProcesso.java
+++ ProjetoNPJ/src/br/unama/npj/Model/ParteProcesso.java
@@ -5,30 +5,39 @@ public class ParteProcesso {
 	private Pessoa pessoa;
 	private Processo processo;
 	private TipoParte tipoparte;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
+
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
+
 	public Processo getProcesso() {
 		return processo;
 	}
+
 	public void setProcesso(Processo processo) {
 		this.processo = processo;
 	}
+
 	public TipoParte getTipoparte() {
 		return tipoparte;
 	}
+
 	public void setTipoparte(TipoParte tipoparte) {
 		this.tipoparte = tipoparte;
 	}
+
 	public ParteProcesso(int id, Pessoa pessoa, Processo processo, TipoParte tipoparte) {
 		super();
 		this.id = id;
--- ProjetoNPJ/src/br/unama/npj/Model/TipoParte.java
+++ ProjetoNPJ/src/br/unama/npj/Model/TipoParte.java
@@ -3,22 +3,27 @@ package br.unama.npj.Model;
 public class TipoParte {
 	private int id;
 	private String descricao;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public TipoParte(int id, String descricao) {
 		super();
 		this.id = id;
 		this.descricao = descricao;
 	}
-	
+
 }
